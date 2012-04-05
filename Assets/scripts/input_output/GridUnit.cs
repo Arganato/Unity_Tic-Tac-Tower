227704ef65ee02f03fc8a3359348a55c6b562bb9
Eivind Karlsen <eivind_gmlmosseveg@hotmail.com>
2012-04-05 14:03:12 +0200

    added rotation animations for pieces and updated graphics

--- Assets/scripts/input_output/GridUnit.cs
+++ Assets/scripts/input_output/GridUnit.cs
@@ -6,8 +6,18 @@ public class GridUnit : MonoBehaviour {
 
 	public Route type;
 	public FieldIndex index;
+	
+	public Vector3 speed;
+	public float speedY;
+	public float speedZ;
 
 	void Awake(){
 		type = Route.outOfBounds;
+		speed = Random.onUnitSphere/4;
 	}
+	
+	void Update(){
+		transform.Rotate(speed);
+	}
+		
 }
