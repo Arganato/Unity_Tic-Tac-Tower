eb609dd9d4d065a713600e49d839fbb995259ae3
Eivind Karlsen <eivind_gmlmosseveg@hotmail.com>
2012-04-06 17:58:30 +0200

    further progress with the tutorial

--- Assets/scripts/GameLogic/Tutorial.cs
+++ Assets/scripts/GameLogic/Tutorial.cs
@@ -5,45 +5,100 @@ public class Tutorial : MonoBehaviour {
 
 	Vector2 scrollPos = new Vector2(0,0);
 	public GUIStyle textBox;
-	public Transform camera;
-	
+	new public Transform camera;
+	public Texture[] towerTextures;
+
 	private Control control;
 	
 	//camera positions
 
-	private Vector3 camPreviewPos = new Vector3(-30,150,35);
-	private Vector3 camPreviewRot = new Vector3(90,0,0);
+	//private Vector3 camPreviewPos = new Vector3(-30,150,35);
+	//private Vector3 camPreviewRot = new Vector3(90,0,0);
 	
-	private float t1;
-	private float t2;
-	private int status = 0;
+	//event points
+	
+	public bool changeChapter = false;
+	public int chapter = 0;
+	//private int section = 0;
+	
+	private int menuWidth = 300;
+	private int menuChangeSpeed = 50; //100px/2sec
+	
+	//text
+	private string introText = "this is a scroll text \n\n\n\n\n\n\n this is further down\n\n\n\n\nthis is even further down\n\n this sentence is so long only because I wanted to see if the line is clipped or not. Probably it is clipped, and that would make me kinda sad.";
+	
+	private string[] towerExpl = new string[3]; //put skill explanations here
 	
 	void Start () {
-		t1 = 5;
-		t2 = 10;
 		control = (Control)FindObjectOfType(typeof(Control));
-		control.StartNewGame();
-		
+		control.StartNewGame();		
 	}
-	
-	void Update () {
-		if ( status == 0 && Time.time > t1){
-			status = 1;
-			camera.animation.Play("anim1");
-		}else if(status == 1 && Time.time > t2){
-			status = 2;
-			camera.animation.Play("anim2");
+	void Update(){
+		if(changeChapter && chapter == 0 && menuWidth > 200){
+			menuWidth -= Mathf.RoundToInt(Time.deltaTime*menuChangeSpeed);
 		}
 	}
+	
 	void OnGUI(){
-		PrintTextWindow();		
+		switch(chapter){
+		case 0:
+			PrintTextWindow();
+			break;
+		case 1:
+			PrintSkillInfoWindow();
+			break;
+		case 2:
+			break;
+		}
 	}
 	
 
 	private void PrintTextWindow(){
-		GUI.BeginScrollView(new Rect(0,100,300, 600),scrollPos, new Rect(0,0,300,800));
-		GUI.Box(new Rect(0,0,300,800),"this is a scroll text \n\n\n\n\n\n\n this is further down\n\n\n\n\nthis is even further down\n\n this sentence is so long only because I wanted to see if the line is clipped or not. Probably it is clipped, and that would make me kinda sad.",textBox);
+		if(GUI.Button(new Rect(200,600,100,25),"Next")){
+			Debug.Log("stufffff..");
+			//SimpleChangeChapter();
+			StartCoroutine(ChangeChapter());
+		}
+
+		GUI.BeginScrollView(new Rect(0,100,menuWidth, 500),scrollPos, new Rect(0,0,menuWidth,800));
+		GUI.Box(new Rect(0,0,menuWidth,800),introText,textBox);
 		GUI.EndScrollView();
+	}
+	private void PrintSkillInfoWindow(){
+		GUI.BeginScrollView(new Rect(0,100,200, 500),scrollPos, new Rect(0,0,200,800));
+		GUI.Box(new Rect(0,0,200,100),towerExpl[0],textBox);
+		GUI.Box(new Rect(0,100,100,100),towerTextures[0]);
+		GUI.EndScrollView();
+		if(GUI.Button(new Rect(100,700,100,25),"Next")){
+			ChangeChapter();
+		}
+	}
+
+	private void SimpleChangeChapter(){
+		Debug.Log("simple change chapter");
+		camera.animation.Play("anim1");
+		chapter++;
+	}
+	
+	private IEnumerator ChangeChapter(){
+		//check for end of section
+		Debug.Log("changing chapter...");
+		changeChapter = true;
+
+		switch(chapter){
+		case 0:
+			camera.animation.Play("anim1");			
+			break;
+		case 1:
+			camera.animation.Play("anim2");	
+			break;
+		case 2:
+			break;
+		}
+
+		yield return new WaitForSeconds(2);
+		changeChapter = false;
+		chapter++;
 	}
 
 }
